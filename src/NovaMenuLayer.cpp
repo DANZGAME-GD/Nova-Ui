a83db20f456c46a6b2e84dfb27d1c70de6e34712
DANZGAME-GD <danzgameff@gmail.com>
2026-06-14 00:33:55 +0700

    move NovaMenuLayer.cpp

--- src/NovaMenuLayer.cpp
+++ src/NovaMenuLayer.cpp
@@ -1,5 +1,7 @@
+#include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
 #include <Geode/loader/Setting.hpp>
+#include "SetupUI.hpp"
 
 using namespace geode::prelude;
 
@@ -20,13 +22,15 @@ class $modify(NovaMenuLayer, MenuLayer) {
         this->addChild(menuva);
 
         //hide menu
-        bool hide = Mod::get()->getSettingValue<bool>("enable-user-interface");
+        bool enableuserinterface = Mod::get()->getSettingValue<bool>("enable-user-interface");
         
         auto playbtn = this->getChildByID("main-menu");
         auto media = this->getChildByID("social-media-menu");
         auto more = this->getChildByID("more-games-menu");
         auto gdtitle = this->getChildByID("main-title");
+        auto profile = this->getChildByID("profile-menu");
 
+        // make button
         auto makeButton = [&](const char* logoframe, const char* text, const char* fnt, const char* idspr, const char* idlogo, const char* idtext,  SEL_MenuHandler cb) {
             auto spr = CCSprite::create("Button.png"_spr);
             auto logo = CCSprite::create(logoframe);
@@ -51,8 +55,7 @@ class $modify(NovaMenuLayer, MenuLayer) {
             spr->addChild(label);
             return CCMenuItemSpriteExtra::create(spr, this, cb);
         };
-
-        // make button
+        
         auto mainlevelbtn = makeButton("main_icon.png"_spr, "Main Levels", "gjFont52.fnt", "main-levels-btn", "main-levels-icon", "main-levels-label",  menu_selector(NovaMenuLayer::onPlay));
         auto creatorbtn = makeButton("creator_point.png"_spr, "Creator", "gjFont52.fnt", "creator-btn", "creator-icon", "creator-label", menu_selector(NovaMenuLayer::onCreator));
         auto iconbtn = makeButton("exit.png"_spr, "Icon", "gjFont52.fnt", "icons-btn", "icons-logo", "icons-label", menu_selector(NovaMenuLayer::onGarage));
@@ -60,6 +63,20 @@ class $modify(NovaMenuLayer, MenuLayer) {
         auto geodebtn = makeButton("geode_logo.png"_spr, "Geode", "gjFont52.fnt", "geoode-btn", "geode-icons", "geode-label", menu_selector(NovaMenuLayer::onOptions));
         auto exitbtn = makeButton("exit.png"_spr, "Quit", "gjFont52.fnt", "exit-btn", "exit-icon", "exit-label", menu_selector(NovaMenuLayer::onQuit));
         
+        auto searchbtn = CCMenuItemSpriteExtra::create(
+            CCSprite::create("search.png"_spr),
+            this,
+            menu_selector(NovaMenuLayer::onOptions)
+        );
+        PositionHelper::hitungPosisiAnchor("center-right", [&](CCPoint pos, CCPoint anchor) {
+            searchbtn->setPosition(pos);
+            searchbtn->setID("search-button");
+            searchbtn->setPositionX(searchbtn->getPositionX() - 108);
+            searchbtn->setPositionY(searchbtn->getPositionY() - 94);
+            searchbtn->setScale(1.8);
+            searchbtn->m_baseScale = 1.8;
+            menuva->addChild(searchbtn);
+        });
         // set position
         mainlevelbtn->setPosition({ 75, winSize.height / 2 + 60 });
         mainlevelbtn->setID("main-levels-button");
@@ -81,13 +98,21 @@ class $modify(NovaMenuLayer, MenuLayer) {
         menuva->addChild(geodebtn);
         menuva->addChild(exitbtn);
 
-if (!Mod::get()->getSettingValue<bool>("enable-user-interface"))
-    menuva->removeAllChildrenWithCleanup(true);
-    playbtn->setVisible(!hide);
-    media->setVisible(!hide);
-    more->setVisible(!hide);
-    gdtitle->setVisible(!hide);
-    return;
+        if (enableuserinterface) {
+            menuva->setVisible(true);
+            playbtn->setVisible(false);
+            media->setVisible(false);
+            more->setVisible(false);
+            gdtitle->setVisible(false);
+            profile->setVisible(true);
+            return;
+        }
+        menuva->setVisible(false);
+        playbtn->setVisible(true);
+        media->setVisible(true);
+        more->setVisible(true);
+        gdtitle->setVisible(true);
+        profile->setVisible(true);
     }
     
     void onPlay(CCObject*) {
